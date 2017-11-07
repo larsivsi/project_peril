1e6ba33f954a2c08d35cefb1d55a7c033e662eb4
Lars-Ivar Hesselberg Simonsen <larsivarhs@gmail.com>
2017-11-08 00:03:37 +0100

    main loop \o/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::time::{Duration, SystemTime};
+//for debug/simulation
+use std::thread::sleep;
+
 fn main() {
-    println!("Hello, world!");
+    // init stuff
+
+    // main loop
+    let mut running = true;
+    let mut framecount: u64 = 0;
+    // aim for 60fps = 16.66666... ms
+    let delta_time = Duration::from_millis(17);
+    let mut elapsed_time = Duration::new(0, 0);
+    let mut accumulator = Duration::new(0, 0);
+    let mut current_time = SystemTime::now();
+
+    while running {
+        let new_time = SystemTime::now();
+        let frame_time = new_time.duration_since(current_time).expect(
+            "duration_since failed :(",
+        );
+        current_time = new_time;
+        accumulator += frame_time;
+
+        while accumulator >= delta_time {
+            //animation, physics engine, scene progression etc. goes here
+            accumulator -= delta_time;
+            elapsed_time += delta_time;
+        }
+
+        //call to render function goes here
+        // (now simulated with a sleep)
+        sleep(Duration::from_millis(10));
+        //then swapbuffers etc.
+        framecount += 1;
+
+        if framecount % 100 == 0 {
+            let frame_time_ms = frame_time.subsec_nanos() as f64 / 1_000_000.0;
+            println!(
+                "frametime: {}ms => {} FPS",
+                frame_time_ms,
+                1_000.0 / frame_time_ms
+            );
+        }
+
+        // need some condition to stop the main loop
+        if framecount == 1000 {
+            running = false;
+        }
+    }
+
+    //cleanup
 }
