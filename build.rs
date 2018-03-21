39a7d6b7e071131520c8dabb28342761dd3e115c
Lars-Ivar Hesselberg Simonsen <larsivarhs@gmail.com>
2018-03-22 00:34:50 +0100

    Builds shaders with rust instead of calling bash
    Because it's cool.

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+extern crate glob;
+
+use glob::glob;
 use std::env;
 use std::process::Command;
 
@@ -9,10 +12,57 @@ fn main()
 	// Change path to the shaders-directory and generate shaders
 	path.push("shaders");
 	env::set_current_dir(&path).unwrap();
-	let output = Command::new("./generate_spv.sh").output().expect("Failed to generate shaders");
-	println!("cargo:rerun-if-changed={}", path.display());
-	println!("{}", output.status);
-	println!("{}", String::from_utf8_lossy(&output.stdout));
+
+	// Build shaders
+	for shader in glob("*.vert").unwrap()
+	{
+		let glsl_name = match shader
+		{
+			Ok(s) => s.into_os_string().into_string().unwrap(),
+			Err(e) => panic!("{:?}", e),
+		};
+		let spv_name = glsl_name.replace(".", "_") + ".spv";
+
+		println!("cargo:warning={} -> {}", glsl_name, spv_name);
+
+		let output = Command::new("glslangValidator")
+			.args(&["-V", glsl_name.as_str(), "-o", spv_name.as_str()])
+			.output()
+			.expect("Could not execute glslangValidator, is it in PATH?");
+
+		if !output.status.success()
+		{
+			panic!("Failed to build shader: {}", String::from_utf8_lossy(&output.stdout));
+		}
+
+		println!("cargo:rerun-if-changed={}", glsl_name);
+		println!("cargo:rerun-if-changed={}", spv_name);
+	}
+
+	for shader in glob("*.frag").unwrap()
+	{
+		let glsl_name = match shader
+		{
+			Ok(s) => s.into_os_string().into_string().unwrap(),
+			Err(e) => panic!("{:?}", e),
+		};
+		let spv_name = glsl_name.replace(".", "_") + ".spv";
+
+		println!("cargo:warning={} -> {}", glsl_name, spv_name);
+
+		let output = Command::new("glslangValidator")
+			.args(&["-V", glsl_name.as_str(), "-o", spv_name.as_str()])
+			.output()
+			.expect("Could not execute glslangValidator, is it in PATH?");
+
+		if !output.status.success()
+		{
+			panic!("Failed to build shader: {}", String::from_utf8_lossy(&output.stdout));
+		}
+
+		println!("cargo:rerun-if-changed={}", glsl_name);
+		println!("cargo:rerun-if-changed={}", spv_name);
+	}
 
 	// Return to first directory
 	path.pop();
--- shaders/generate_spv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for shader in {*.vert,*.frag}; do
-	output_name="${shader/./_}.spv"
-	echo "$shader -> $output_name"
-	glslangValidator -V $shader -o $output_name
-done
