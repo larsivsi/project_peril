34aab8931a54f7966a73ba9784f4299ca08f4aa1
Lars-Ivar Hesselberg Simonsen <larsivarhs@gmail.com>
2019-09-12 23:11:23 +0200

    Cleans up glsl shader compilation
    This commit cleans up build.rs and ensures that shaders are only rebuilt
    when changed.

--- build.rs
+++ build.rs
@@ -2,66 +2,48 @@ extern crate glob;
 
 use glob::glob;
 use std::env;
+use std::path;
 use std::process::Command;
+
+fn compile_shader(shaderpath: path::PathBuf)
+{
+	let glsl_name = shaderpath.into_os_string().into_string().unwrap();
+	let spv_name = glsl_name.replace(".", "_") + ".spv";
+
+	let output = Command::new("glslangValidator")
+		.args(&["-V", glsl_name.as_str(), "-o", spv_name.as_str()])
+		.output()
+		.expect("Could not execute glslangValidator, is it in PATH?");
+
+	if !output.status.success()
+	{
+		panic!("Failed to build shader: {}", String::from_utf8_lossy(&output.stdout));
+	}
+}
 
 fn main()
 {
+	let shaderdir = "shaders";
+
+	// Only run if shaders have changed
+	println!("cargo:rerun-if-changed={}", shaderdir);
+
 	// Get CWD
 	let mut path = env::current_dir().unwrap();
 
 	// Change path to the shaders-directory and generate shaders
-	path.push("shaders");
+	path.push(shaderdir);
 	env::set_current_dir(&path).unwrap();
 
 	// Build shaders
-	for shader in glob("*.vert").unwrap()
+	for vertpath in glob("*.vert").unwrap().filter_map(Result::ok)
 	{
-		let glsl_name = match shader
-		{
-			Ok(s) => s.into_os_string().into_string().unwrap(),
-			Err(e) => panic!("{:?}", e),
-		};
-		let spv_name = glsl_name.replace(".", "_") + ".spv";
-
-		println!("cargo:warning={} -> {}", glsl_name, spv_name);
-
-		let output = Command::new("glslangValidator")
-			.args(&["-V", glsl_name.as_str(), "-o", spv_name.as_str()])
-			.output()
-			.expect("Could not execute glslangValidator, is it in PATH?");
-
-		if !output.status.success()
-		{
-			panic!("Failed to build shader: {}", String::from_utf8_lossy(&output.stdout));
-		}
-
-		println!("cargo:rerun-if-changed={}/{}", path.display(), glsl_name);
-		println!("cargo:rerun-if-changed={}/{}", path.display(), spv_name);
+		compile_shader(vertpath);
 	}
 
-	for shader in glob("*.frag").unwrap()
+	for fragpath in glob("*.frag").unwrap().filter_map(Result::ok)
 	{
-		let glsl_name = match shader
-		{
-			Ok(s) => s.into_os_string().into_string().unwrap(),
-			Err(e) => panic!("{:?}", e),
-		};
-		let spv_name = glsl_name.replace(".", "_") + ".spv";
-
-		println!("cargo:warning={} -> {}", glsl_name, spv_name);
-
-		let output = Command::new("glslangValidator")
-			.args(&["-V", glsl_name.as_str(), "-o", spv_name.as_str()])
-			.output()
-			.expect("Could not execute glslangValidator, is it in PATH?");
-
-		if !output.status.success()
-		{
-			panic!("Failed to build shader: {}", String::from_utf8_lossy(&output.stdout));
-		}
-
-		println!("cargo:rerun-if-changed={}/{}", path.display(), glsl_name);
-		println!("cargo:rerun-if-changed={}/{}", path.display(), spv_name);
+		compile_shader(fragpath);
 	}
 
 	// Return to first directory
